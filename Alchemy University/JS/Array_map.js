a150b2066760e7281b9806d27fe392eac64be9b0
Fushiguro19 <86782830+Fushiguro19@users.noreply.github.com>
2022-12-12 13:53:23 +0530

    Update Array_map.js

--- Alchemy University/JS/Array_map.js
+++ Alchemy University/JS/Array_map.js
@@ -13,3 +13,43 @@ function squared(n) {
 function squaredMap(arr) {
     return arr.map(squared);
 }
+
+
+// Given an array of players, add 10 to their score. Return a new array with these additional points.
+// Each player will be an object with the following two properties:
+// id - a number uniquely identifying the player
+// score - a number representing the player's points
+
+
+function addScore(players) {
+    return players.map(function(x){
+        return {
+            id : x.id,
+            score : x.score+10
+        }
+    });
+}
+
+
+//Accessing elements index using map
+[1,2,3].map(function(x,i){
+    console.log(i);});
+
+
+//Add 10 to score of players with id less than 3
+const players = [{id : 0, score : 10},
+{id : 1, score : 20}, {id : 2, score : 5}, {id : 3, score : 15}];
+
+console.log(players.map(function(x,y){
+    if(y<3){
+        return {
+            id : x.id,
+            score : x.score+10
+        };
+    }else{
+        return{
+            id : x.id,
+            score : x.score
+        };
+    }
+}));
